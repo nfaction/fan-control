216617a758501857b6a9ea6ce309fc4c12011402
Matt DePorter <spikebyte@gir.lan>
2015-03-04 01:11:35 -0700

    Added tempurature code

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,7 +1,6 @@
 #include <stdbool.h>
+#include "DHT.h"
 // Pin connections to board
-
-
 
 // Light
 const int light = 12;
@@ -22,6 +21,11 @@ const int f3 = 6;
 const int f2 = 5;
 // Fan 1 (living room)
 const int f1 = 4;
+
+#define DHTPIN 2     // DHT humiture pin
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+// Initialize DHT sensor for normal 16mhz Arduino
+DHT dht(DHTPIN, DHTTYPE);
 
 // Stores serial input one character at a time
 byte input = 0;
@@ -116,6 +120,37 @@ void loop() {
       digitalWrite(fan, LOW);
       delay(300);
       
+      // Get Tempurature
+      // Reading temperature or humidity takes about 250 milliseconds!
+      // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+      float h = dht.readHumidity();
+      // Read temperature as Celsius
+      float t = dht.readTemperature();
+      // Read temperature as Fahrenheit
+      float f = dht.readTemperature(true);
+      
+      // Check if any reads failed and exit early (to try again).
+      if (isnan(h) || isnan(t) || isnan(f)) {
+        Serial.println("Failed to read from DHT sensor!");
+        return;
+      }
+
+      // Compute heat index
+      // Must send in temp in Fahrenheit!
+      float hi = dht.computeHeatIndex(f, h);
+
+      Serial.print("Humidity: "); 
+      Serial.print(h);
+      Serial.print(" %\t");
+      Serial.print("Temperature: "); 
+      Serial.print(t);
+      Serial.print(" *C ");
+      Serial.print(f);
+      Serial.print(" *F\t");
+      Serial.print("Heat index: ");
+      Serial.print(hi);
+      Serial.println(" *F");
+  
       input = Serial.read();
       
       if(input == 's'){
